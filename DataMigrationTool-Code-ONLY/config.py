b36c3ab646c1ddecfa55d84026f7dcc057e5f48b
Munir93 <37178013+Munir93@users.noreply.github.com>
2018-07-20 23:47:59 +0100

    Add files via upload

--- DataMigrationTool-Code-ONLY/config.py
+++ DataMigrationTool-Code-ONLY/config.py
@@ -1,12 +1,11 @@
-
-
+import time
+from time import gmtime, strftime
 
 # Giving access to use client library
 #for the purpose of migration user needs to be granted the following access levels
 # bigquery.admin and storage.objectViewer
 
-ACCOUNT_NAME = 'munirs_account'
-
+'''gs-bg-migration i.e dynamic load'''
 PROJECT_ID ='warm-airline-207713'
 
 KEY_PATH = 'authkey1.json'
@@ -25,7 +24,7 @@ STORAGE_KEY_PATH = 'storage_key.json'
 
 #can create function to auto genrate list of files directly from bucket using list blobs method
 #ALL_files =
-LIST_OF_FILES = ['SampleCSVFile_119kb.csv', 'SampleCSVFile_556kb.csv', 'sample_with_headers.csv', 'blaaah',]
+#LIST_OF_FILES = ['SampleCSVFile_119kb.csv', 'SampleCSVFile_556kb.csv', 'sample_with_headers.csv', 'blaaah',]
 
 #header available = yes/no
 
@@ -33,5 +32,30 @@ LIST_OF_FILES = ['SampleCSVFile_119kb.csv', 'SampleCSVFile_556kb.csv', 'sample_w
 
 LOGGING_FILENAME = 'DoD-Project1-logs1.log'
 
+'''static-migrator  i.e batch load'''
+
+MASTER_TABLE = 'Brexit_Tweets'
 
 
+'''Twitter ETL start STREAM '''
+
+TRACK_TERMS = ["brexit"]
+
+TWITTER_APP_KEY = 'KPVEb25geZ1WUxMrJz3flS6je'
+TWITTER_APP_SECRET = 'BcjmYWEJmDuU81gwW3Hs64mnzH4I67EzgS0H5FfuIs7VFjkXtH'
+TWITTER_KEY = '1014592058215059456-Z1pGlZHdMvx8vsszkDWpsEdTCjfz4o'
+TWITTER_SECRET = 'Zsw17mh9OiXDfaf993njgHhGZHxYTHrrD9Ae8fgqEc1mq'
+
+
+time = strftime("%Y_%m_%d_%H_%M_%S", gmtime())
+
+CSV_NAME = 'Tweets'+time+'.csv'
+COLUMN_NAMES = ['Username', 'Tweet', 'Time', 'Followers', 'Location', 'Source']
+
+
+
+'''Twitter ETL start historical-tweets'''
+
+TOPIC = 'Brexit_Tweets'
+TRACK_TERMS = 'brexit'
+COLUMN_NAMES = ['Username', 'Tweet', 'Time', 'Followers', 'Location', 'Source']
