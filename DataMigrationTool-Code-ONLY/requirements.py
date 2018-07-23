6001f2746bfe11fb9f75bf21c89b77d6ff0185e4
Munir93 <37178013+Munir93@users.noreply.github.com>
2018-07-23 17:15:58 +0100

    Add files via upload

---
+++ DataMigrationTool-Code-ONLY/requirements.py
@@ -0,0 +1,10 @@
+import os 
+
+os.system('sudo apt-get -y install python-pip')
+
+os.system('sudo pip install google.cloud')
+
+os.system('sudo pip install logging')
+
+os.system('git clone https://github.com/Munir93/gcloudpractice')
+
