34b1efca265f0f2f511bd1d783645a381baff036
Munir93 <37178013+Munir93@users.noreply.github.com>
2018-07-26 17:56:02 +0100

    Add files via upload

--- DataMigrationTwitterETL-Code-ONLY/twitter-ETL.py
+++ DataMigrationTwitterETL-Code-ONLY/twitter-ETL.py
@@ -1,4 +1,3 @@
-
 import tweepy
 # for connection to google biqQ later pip install --upgrade google-cloud-bigquery
 # https://cloud.google.com/bigquery/docs/reference/libraries#client-libraries-install-python
@@ -16,20 +15,18 @@ from google.cloud import bigquery, storage
 from faker import Faker as fk
 '''Creating the client to use with GCS - json key required if running from external application'''
 
-storage_client = storage.Client()
-#.from_service_account_json(json_credentials_path=config.STORAGE_KEY_PATH,project=config.PROJECT_ID)
+storage_client = storage.Client.from_service_account_json(json_credentials_path=config.STORAGE_KEY_PATH,project=config.PROJECT_ID)
 bucket = storage_client.get_bucket(config.BUCKET_NAME)
-import config
-from google.cloud import pubsub
-import os
 
-
+autogen = fk('en_GB')
+#autogen.address()
 '''Steam Listener sublcassed from the tweepy module class Stream Listener'''
 class StreamListener(tweepy.StreamListener):
     def __init__(self, time_limit=60):
         self.start_time = time.time()
         self.limit = time_limit
         super(StreamListener, self).__init__()
+    '''Created some class variables to use for the timer'''
 
     '''Overiding the on_status method see tweepy documentation '''
     def on_status(self, status):
@@ -51,16 +48,11 @@ class StreamListener(tweepy.StreamListener):
         source = ct.r_source()
         #print(type(created), created)
         # here we need to ocnstruct the final line of data and send each line to a csv that will remain in the folder
-        line = [name.encode('utf-8'), text.encode('utf-8'), created, followers, location.encode('utf-8'), source.encode('utf-8')]
-        '''Publiah message to topic'''
-        publine = ','.join(map(str, line))
-        publish_line = publine.encode('utf-8')
-        publisher.publish(topic_path, publish_line)
+        line = [name, text.encode('utf-8'), created, followers, location, source]
 
-
-   #     with open(config.CSV_NAME, 'a') as f:
-    #        line_writer = csv.writer(f)
-     #       line_writer.writerow(line)
+        with open(config.CSV_NAME, 'a') as f:
+            line_writer = csv.writer(f)
+            line_writer.writerow(line)
         # returning false if the time limit runs out thus stopping the stream safely
         if (time.time() - self.start_time) < self.limit:
             return True
@@ -80,17 +72,6 @@ def send_to_GCS(csv):
 print('File {} uploaded to {}.'.format(csv,config.BUCKET_NAME))
 
 if __name__ == '__main__':
- #   os.environ[
-  #      "GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/709231/PycharmProjects/DataMigrationProjectGCP/pubsub-with-storage.json"
-
-    '''Create publisher client'''
-    publisher = pubsub.PublisherClient()
-
-    '''Set topic path with topic name and Project id'''
-    topic_path = publisher.topic_path(config.PROJECT_ID, 'twitter-stream')
-
-    '''Create initial topic'''
-    #publisher.create_topic(topic_path)
 
 
     auth = tweepy.OAuthHandler(config.TWITTER_APP_KEY, config.TWITTER_APP_SECRET)
@@ -102,9 +83,9 @@ if __name__ == '__main__':
     stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
     stream.filter(track=config.TRACK_TERMS)
 
-    #df = read_csv(config.CSV_NAME)
-    #df.columns = config.COLUMN_NAMES
-    #df.to_csv(config.CSV_NAME,index=False)
+    df = read_csv(config.CSV_NAME)
+    df.columns = config.COLUMN_NAMES
+    df.to_csv(config.CSV_NAME,index=False)
     #send_to_GCS(config.CSV_NAME)
     #os.system('gsutil cp' +config.CSV_NAME+' gs://'+config.BUCKET_NAME+'/Source/')
 
