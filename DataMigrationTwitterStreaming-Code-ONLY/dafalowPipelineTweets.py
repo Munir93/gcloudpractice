5b35d82e6045bff5405ee11922a78606a70ed297
Munir93 <37178013+Munir93@users.noreply.github.com>
2018-08-05 14:17:29 +0100

    Add files via upload

--- DataMigrationTwitterStreaming-Code-ONLY/dafalowPipelineTweets.py
+++ DataMigrationTwitterStreaming-Code-ONLY/dafalowPipelineTweets.py
@@ -4,28 +4,46 @@ import config
 from google.cloud import pubsub
 import os
 import argparse
+import json
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import GoogleCloudOptions
+from apache_beam.options.pipeline_options import StandardOptions
+from apache_beam.options.pipeline_options import SetupOptions
+from apache_beam.io.gcp.internal.clients import bigquery
+
+options = PipelineOptions()
+
+google_cloud_options = options.view_as(GoogleCloudOptions)
+google_cloud_options.project = config.PROJECT_ID
+options.view_as(StandardOptions).runner = 'DataflowRunner'
+options.view_as(StandardOptions).streaming = True
+
+
 def parse_pubsub(line):
     import json
     record = json.loads(line)
     return (record['Name']), (record['Tweet']), (record['Time']), (record['Followers']), (record['Location']), (record['Device'])
 
+def handle_pubsub(line):
+    templist = line.split('-=-')
+    for j, item in enumerate(templist):
+        templist[j] = item.replace(',', '')
+
+    diction = dict(zip(config.COLUMN_NAMES, templist))
+    f = json.dumps(diction)
+    return f
+#    | beam.Map(lambda Name_bq, Tweet_bq, Time_bq, Followers_bq, Location_bq, Device_bq: {'Name':Name_bq , 'Tweet':Tweet_bq, 'Time': Time_bq, 'Followers':Followers_bq, 'Location':Location_bq, 'Source':Device_bq})
 
 def run(argv=None):
-    """Build and run the pipeline."""
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input_topic', required=True,help='Input PubSub topic of the form "/topics/<PROJECT>/<TOPIC>".')
-    parser.add_argument('--output_table', required=True,help=('Output BigQuery table for results specified as: PROJECT:DATASET.TABLE '
-       'or DATASET.TABLE.'))
-    known_args, pipeline_args = parser.parse_known_args(argv)
 
-    with beam.Pipeline(argv=pipeline_args) as p:
+
+
+    with beam.Pipeline(options=options) as p:
         # Read the pubsub topic into a PCollection.
-        lines = (p | beam.io.ReadStringsFromPubSub(known_args.input_topic)
-                   | beam.Map(parse_pubsub)
-                   | beam.Map(lambda Name_bq, Tweet_bq, Time_bq, Followers_bq, Location_bq, Device_bq: {'Name':Name_bq , 'Tweet':Tweet_bq, 'Time': Time_bq, 'Followers':Followers_bq, 'Location':Location_bq, 'Device':Device_bq})
-                   | beam.io.WriteToBigQuery(
-                    known_args.output_table,
+        lines = (p | beam.io.ReadStringsFromPubSub(topic='projects/warm-airline-207713/topics/twitter-stream')
+                   | beam.Map(handle_pubsub)
+                   | beam.io.WriteToBigQuery('warm-airline-207713:Tweets_raw.Donald_Trump_Tweets',
                     schema='Username:STRING, Tweet:STRING, Time:TIMESTAMP, Followers:INTEGER, Location:STRING, Source:STRING',
                     create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
                     write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND))
