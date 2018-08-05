c8032668022ca14d129da06b5db57cae94edf9fa
Munir93 <37178013+Munir93@users.noreply.github.com>
2018-08-05 18:15:30 +0100

    Update dafalowPipelineTweets.py

--- DataMigrationTwitterStreaming-Code-ONLY/dafalowPipelineTweets.py
+++ DataMigrationTwitterStreaming-Code-ONLY/dafalowPipelineTweets.py
@@ -8,7 +8,7 @@ from apache_beam.options.pipeline_options import GoogleCloudOptions
 from apache_beam.options.pipeline_options import StandardOptions
 from apache_beam.options.pipeline_options import SetupOptions
 from apache_beam.io.gcp.internal.clients import bigquery
-from textblob import TextBlob
+
 
 options = PipelineOptions()
 
@@ -23,19 +23,19 @@ options.view_as(StandardOptions).streaming = True
 
 
 def compute_sentiment(line):
-    import os 
-    os.system('sudo pip install textblob')
+   # import os 
+    #os.system('sudo pip install textblob')
     
-    import textblob
-    from textblob import TextBlob
+   # import textblob
+   # from textblob import TextBlob
     templist = line.split('-=-')
     for j, item in enumerate(templist):
         templist[j] = item.replace(',', '')
-    tweet = templist[1]
-    sent = TextBlob(tweet).sentiment.polarity
-    templist.append(str(sent))
+   # tweet = templist[1]
+    #sent = TextBlob(tweet).sentiment.polarity
+    #templist.append(str(sent))
 
-    diction = dict(zip(['Username', 'Tweet', 'Time', 'Followers', 'Location', 'Source', 'Sentiment'], templist))
+    diction = dict(zip(['Username', 'Tweet', 'Time', 'Followers', 'Location', 'Source'], templist))
 
     return diction
 
@@ -48,8 +48,8 @@ def run(argv=None):
         # Read the pubsub topic into a PCollection.
         lines = (p | beam.io.ReadStringsFromPubSub(topic='projects/warm-airline-207713/topics/twitter-stream')
                    | beam.Map(compute_sentiment)
-                   | beam.io.WriteToBigQuery('warm-airline-207713:Tweets_raw.Donald_Trump_Tweets_DS',
-                    schema='Username:STRING, Tweet:STRING, Time:TIMESTAMP, Followers:INTEGER, Location:STRING, Source:STRING, Sentiment:FLOAT',
+                   | beam.io.WriteToBigQuery('warm-airline-207713:Tweets_raw.Donald_Trump_Tweets',
+                    schema='Username:STRING, Tweet:STRING, Time:TIMESTAMP, Followers:INTEGER, Location:STRING, Source:STRING',
                     create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
                     write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND))
 
